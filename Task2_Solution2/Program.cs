dbaa034bc0d8f90b2d2cb0c2cd034407198940af
Alex Kizilov <as_k@list.ru>
2022-11-13 23:09:04 +0400

    Duplicate

---
+++ Task2_Solution2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+//Для решения задания использование цикла for является обязательным условием
+
+Console.Clear();
+int GetSumNumbers(int n)
+{
+    n = Math.Abs(n);    
+    string nStr = Convert.ToString(n);
+    int length = nStr.Length;
+    int res = 0;
+    for (int i = 0; i < length; i++)
+    {
+        res += Convert.ToInt32(nStr[i].ToString());
+    }
+    return res;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = GetSumNumbers(number);
+Console.WriteLine();
+Console.WriteLine($"Сумма цифр в числе: {number} = {sum}");
+Console.WriteLine();
